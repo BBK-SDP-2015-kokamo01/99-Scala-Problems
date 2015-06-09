b2562ebcea7de6cdf66d631d847abb2a27f3f31a
keimi <keimiokamoto@gmail.com>
2015-06-09 03:14:38 +0100

    initial commit

---
+++ src/main/scala/list/ListPractise.scala
@@ -0,0 +1,69 @@
+package list
+
+
+object ListPractise {
+  def apply = new ListPractise
+}
+
+class ListPractise {
+
+  /**
+   *01. Find the last element of the list
+   */
+  def getLastElementIn[A](list : List[A]): Option[A] = list match {
+    case Nil => throw new NoSuchElementException
+    case hd::Nil => Some(hd)
+    case hd::tl => getLastElementIn(tl)
+  }
+
+  /**
+   *02. Find the last but one element of a list.
+   */
+  def getPenultimate(list : List[Int]):Option[Int] = list match {
+    case hd::Nil => throw new NoSuchElementException
+    //case Nil => throw new NoSuchElementException
+    case hd::_::Nil => Some(hd)
+    case hd::tl => getPenultimate(tl)
+  }
+
+  /**
+   * 03. Find the Nth element.
+   */
+  def getNthElement[A](n : Int, list: List[A]): Option[A] = list match {
+    case Nil => throw new UnsupportedOperationException
+    case hd::tl => {
+      if (n == 0) throw new NoSuchElementException
+      else if (n == 1) Some(hd)
+      else getNthElement(n-1, tl)
+    }
+  }
+
+  /**
+   * 04. Find the number of elements in a list.
+   */
+  def getNumberOfElements[A](list: List[A]):Int = list match {
+    case Nil => 0
+    case hd::tl => getNumberOfElements(tl) + 1
+  }
+
+  /**
+   *05. Reverse the contents of the list.
+   */
+  def reverse[A](list: List[A]):List[A] = list match {
+    case Nil => Nil
+    case hd::Nil => List(hd)
+    case hd::tl => reverse(tl):+hd
+  }
+
+  /**
+   *06. Find out if the list is a palindrome.
+   */
+  def isPalindrome[A](list: List[A]):Boolean = list match {
+    case Nil => true
+    case hd::Nil => true
+    case hd::tl => {
+      if (hd == tl.last) isPalindrome(tl.take(tl.size-1))
+      else false
+    }
+  }
+}
---
+++ src/test/scala/ListPractiseSpec.scala
@@ -0,0 +1,60 @@
+import list.ListPractise
+import org.scalatest.{Matchers, FlatSpec}
+
+class ListPractiseSpec extends FlatSpec with Matchers{
+  val listPractise = new ListPractise
+  val list = List(1,2,3,4)
+  val listSingleElement = List(1)
+
+
+  "A list" should "get the last element" in {
+    listPractise.getLastElementIn(list) should be (Some(4))
+  }
+  it should "throw NoSuchElementException if the list has no elements" in {
+    a [NoSuchElementException] should be thrownBy {
+      listPractise.getLastElementIn(List())
+    }
+  }
+
+  "A list" should "get the penultimate element" in {
+    listPractise.getPenultimate(list) should be (Some(3))
+  }
+  it should "throw NoSuchElementException if the list has no penultimate element" in {
+    a [NoSuchElementException] should be thrownBy {
+      listPractise.getPenultimate(listSingleElement)
+      listPractise.getPenultimate(List())
+    }
+  }
+
+  "A list" should "find the Nth element" in {
+    listPractise.getNthElement(3,list) should be (Some(3))
+  }
+  it should "throw NoSuchElement if 0 is entered" in {
+    a [NoSuchElementException] should be thrownBy {
+      listPractise.getNthElement(0,list)
+    }
+  }
+  it should "throw UnsupportedOperationException" in {
+    a [UnsupportedOperationException] should be thrownBy {
+      listPractise.getNthElement(5,List())
+    }
+  }
+
+  "A list" should "get the number of elements " in {
+    listPractise.getNumberOfElements(list) should be (4)
+    listPractise.getNumberOfElements(List()) should be (0)
+  }
+
+  "A list" should "reverse the list " in {
+    listPractise.reverse(list) should be (List(4,3,2,1))
+    listPractise.reverse(List()) should be (Nil)
+  }
+
+  "A list" should "return true if it is a palindrome" in {
+    listPractise.isPalindrome(List(1,2,3,2,1)) should be (true)
+    listPractise.isPalindrome(List(1,2,3,3,2,1)) should be (true)
+    listPractise.isPalindrome(List(1,3,3,3,2,1)) should be (false)
+    listPractise.isPalindrome(List()) should be (true)
+  }
+
+}
